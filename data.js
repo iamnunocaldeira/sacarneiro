f7aabdcf29fa44ef9f7e0eae879bfe46017717a3
Nuno Caldeira <nunocapelocaldeira@gmail.com>
2025-11-26 11:00:38 +0000

    upload

---
+++ data.js
@@ -0,0 +1,422 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-distante-1",
+      "name": "Distante 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.01924554885180285,
+        "pitch": 0.30445889183494756,
+        "fov": 1.2413628583426264
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3993769811178485,
+          "pitch": 0.4315014957655876,
+          "rotation": 5.497787143782138,
+          "target": "1-distante-2"
+        },
+        {
+          "yaw": -1.003383239323142,
+          "pitch": 0.5936253636165727,
+          "rotation": 0,
+          "target": "4-prximo-1"
+        },
+        {
+          "yaw": 0.5828020288070199,
+          "pitch": 0.7787980065949434,
+          "rotation": 0,
+          "target": "5-prximo-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-distante-2",
+      "name": "Distante 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.03766897666174884,
+        "pitch": 0.24363450145289,
+        "fov": 1.2413628583426264
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3419288870917008,
+          "pitch": 0.4404966745497205,
+          "rotation": 4.71238898038469,
+          "target": "2-distante-3"
+        },
+        {
+          "yaw": -0.667902357256807,
+          "pitch": 0.7078373303328185,
+          "rotation": 0,
+          "target": "6-prximo-3"
+        },
+        {
+          "yaw": 0.1758811925894186,
+          "pitch": 0.7826863762199316,
+          "rotation": 0,
+          "target": "7-prximo-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-distante-3",
+      "name": "Distante 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.13929813840638694,
+        "pitch": 0.2920122117918851,
+        "fov": 1.2413628583426264
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.267372078124433,
+          "pitch": 0.2804351396385538,
+          "rotation": 4.71238898038469,
+          "target": "3-distante-4"
+        },
+        {
+          "yaw": -0.5974982590184794,
+          "pitch": 0.6962496122269677,
+          "rotation": 0,
+          "target": "8-prximo-5"
+        },
+        {
+          "yaw": 0.3169672386189468,
+          "pitch": 0.7433299697080322,
+          "rotation": 0,
+          "target": "9-prximo-6"
+        },
+        {
+          "yaw": 0.7903947855997089,
+          "pitch": 0.4716727151687259,
+          "rotation": 5.497787143782138,
+          "target": "10-prximo-7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-distante-4",
+      "name": "Distante 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.3371327817556775,
+        "pitch": 0.23276972625820314,
+        "fov": 1.2413628583426264
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.24810776861619033,
+          "pitch": 0.7462328450428348,
+          "rotation": 0,
+          "target": "10-prximo-7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-prximo-1",
+      "name": "Próximo 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.07276427865584267,
+        "pitch": 0.09751732595396945,
+        "fov": 1.2413628583426264
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-prximo-2",
+      "name": "Próximo 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.1563262531462577,
+        "pitch": 0.07579740045201078,
+        "fov": 1.2413628583426264
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-prximo-3",
+      "name": "Próximo 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.31453499291476206,
+        "pitch": 0.008121150048429726,
+        "fov": 1.2413628583426264
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-prximo-4",
+      "name": "Próximo 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.039552425494836285,
+        "pitch": 0,
+        "fov": 1.2413628583426264
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-prximo-5",
+      "name": "Próximo 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.06780415799114792,
+        "pitch": 0.01353525008071621,
+        "fov": 1.2413628583426264
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-prximo-6",
+      "name": "Próximo 6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.29005048941727907,
+        "pitch": 0.04331280025829187,
+        "fov": 1.2413628583426264
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-prximo-7",
+      "name": "Próximo 7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.02636828366322419,
+        "pitch": 0.06226215037129457,
+        "fov": 1.2413628583426264
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Talude Sá Carneiro 11-10-2025",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
